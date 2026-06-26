f210ea1daaccdedb114e3c15cd4ee106803fc3d4
marcor433d <20041239@itdurango.edu.mx>
2026-06-25 19:56:43 -0600

    first commit

---
+++ erp-lite/src/test/java/com/cursospring4/erp_lite/ErpLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring4.erp_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
